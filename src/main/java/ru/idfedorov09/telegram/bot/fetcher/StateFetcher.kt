1365c28e2e8aa9f59510c0f42d33b6154042211c
The_Prax <qwerty202020101010@gmail.com>
2023-09-24 23:45:59 +0300

    board

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/StateFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/StateFetcher.kt
@@ -2,6 +2,8 @@ package ru.idfedorov09.telegram.bot.fetcher
 
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
+import org.telegram.telegrambots.meta.api.objects.InputFile
 import ru.idfedorov09.telegram.bot.data.model.UserResponse
 import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
 import ru.idfedorov09.telegram.bot.flow.ExpContainer
@@ -30,6 +32,15 @@ class StateFetcher(
             val endGameTime = LocalDateTime.parse(startGameTime, formatter).plusMinutes(30)
             val timeDifference = Duration.between(LocalDateTime.now(ZoneId.of("Europe/Moscow")), endGameTime)
             answerMessage += String.format("\nОставшееся время игры: %d минут %d секунд", timeDifference.toMinutes(), timeDifference.seconds % 60)
+
+            val photoHash = userResponse.initiatorTeam?.lastBoardCache ?: return
+
+            bot.execute(
+                SendPhoto().also {
+                    it.chatId = tui
+                    it.photo = InputFile(photoHash)
+                },
+            )
         }
         bot.execute(SendMessage(tui, answerMessage))
     }

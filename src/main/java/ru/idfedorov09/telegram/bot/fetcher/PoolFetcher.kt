bfe8d3facd011b375a159a3f67b1f39fecd0f232
The_Prax <qwerty202020101010@gmail.com>
2023-09-24 14:47:48 +0300

    Team испрвления

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/PoolFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/PoolFetcher.kt
@@ -1,12 +1,10 @@
 package ru.idfedorov09.telegram.bot.fetcher
 
 import org.springframework.stereotype.Component
-import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethodMessage
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import ru.idfedorov09.telegram.bot.data.model.UserResponse
 import ru.idfedorov09.telegram.bot.data.repo.ProblemRepository
 import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
-import ru.idfedorov09.telegram.bot.executor.TelegramWebhookBot
 import ru.idfedorov09.telegram.bot.flow.InjectData
 import ru.idfedorov09.telegram.bot.service.UserInfoService
 
@@ -22,7 +20,7 @@ class PoolFetcher(
     @InjectData
     fun doFetch(
         userResponse: UserResponse,
-        bot: TelegramPollingBot
+        bot: TelegramPollingBot,
     ) {
         val problemsPool = userResponse.initiatorTeam?.problemsPool ?: return
         var resultString = ""
@@ -31,7 +29,6 @@ class PoolFetcher(
             resultString += "${problem.category} ${problem.cost}\n"
         }
         val tui = userResponse.initiator.tui ?: return
-        bot.execute(SendMessage(tui,resultString))
+        bot.execute(SendMessage(tui, resultString))
     }
-
 }

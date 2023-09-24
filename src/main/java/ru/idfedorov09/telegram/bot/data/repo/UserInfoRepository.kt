e3db197c3bbeddcff5cad7c8f48b2288948d069c
Ivan Fedorov <idfedorov09@gmail.com>
2023-09-24 12:10:11 +0300

    finish validate (without reply type) and add userInfoService

--- src/main/java/ru/idfedorov09/telegram/bot/data/repo/UserInfoRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/repo/UserInfoRepository.kt
@@ -3,4 +3,6 @@ package ru.idfedorov09.telegram.bot.data.repo
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.idfedorov09.telegram.bot.data.model.UserInfo
 
-interface UserInfoRepository : JpaRepository<UserInfo, Long>
+interface UserInfoRepository : JpaRepository<UserInfo, Long> {
+    fun findByTui(tui: String): UserInfo?
+}
---
+++ src/main/java/ru/idfedorov09/telegram/bot/service/UserInfoService.kt
@@ -0,0 +1,12 @@
+package ru.idfedorov09.telegram.bot.service
+
+import org.springframework.stereotype.Component
+import ru.idfedorov09.telegram.bot.data.model.UserInfo
+import ru.idfedorov09.telegram.bot.data.repo.TeamRepository
+
+@Component
+class UserInfoService(
+    private val teamRepository: TeamRepository,
+) {
+    fun getTeam(userInfo: UserInfo) = userInfo.teamId?.let { teamRepository.findById(it) }
+}

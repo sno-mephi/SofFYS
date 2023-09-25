970164e49d5aef43f8b4ff8ee557831bfd442306
andromedagalua <andromedagalua@yandex.ru>
2023-09-25 20:27:43 +0300

    чота добавил

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/NewProblemFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/NewProblemFetcher.kt
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.User
 import ru.idfedorov09.telegram.bot.data.enums.ResponseAction
 import ru.idfedorov09.telegram.bot.data.model.UserResponse
+import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
 import ru.idfedorov09.telegram.bot.flow.ExpContainer
 import ru.idfedorov09.telegram.bot.flow.InjectData
 import ru.idfedorov09.telegram.bot.service.RedisService
@@ -14,26 +15,32 @@ class NewProblemFetcher : GeneralFetcher() {
 
     @InjectData
     fun doFetch(
-        userResponse: UserResponse
+        userResponse: UserResponse,
+        bot: TelegramPollingBot,
     ){
         if (userResponse.action != ResponseAction.SELECT_PROBLEM) return
         val user = userResponse.initiator
+        val tui = userResponse.initiator.tui ?: return
         val problemId = userResponse.problemId
-        val team = user.team()?: return
+        val team = userResponse.initiatorTeam?: return
 
         if (problemId in team.problemsPool){
+            bot.execute(SendMessage(tui, "Эта задача уже есть в пуле"))
             // Пишу что задача уже есть в пуле и отправляю ее картинку
             return
         }
         if (!user.isCaptain) return
         if (problemId in team.completedProblems){
+            bot.execute(SendMessage(tui, "Вы уже решали эту задачу"))
             // Пишу что задача уже решена
             return
         }
         if (team.problemsPool.size == 3){
+            bot.execute(SendMessage(tui, "Вы не можете решать больше трех задач одновременно"))
             // Пишу что пул заполнен
             return
         }
+
         // Добавляю задачу в пул
         // Отправляю сообщение с картинкой через хэш из таблицы проблемс ВСЕМ УЧАСТНИКАМ
         // Меняю на борде

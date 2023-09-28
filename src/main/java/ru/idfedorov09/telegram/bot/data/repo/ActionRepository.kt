5c47fa68f5353860b226e6939a49eaff1009a196
andromedagalua <andromedagalua@yandex.ru>
2023-09-28 04:20:25 +0300

    добавил запросы в ActionRepository

--- src/main/java/ru/idfedorov09/telegram/bot/data/repo/ActionRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/repo/ActionRepository.kt
@@ -3,6 +3,23 @@
 package ru.idfedorov09.telegram.bot.data.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import ru.idfedorov09.telegram.bot.data.model.Action
 
-interface ActionRepository : JpaRepository<Action, Long>
+interface ActionRepository : JpaRepository<Action, Long> {
+
+    @Query(
+        "SELECT COUNT(a) " +
+            "FROM actions a " +
+            "WHERE a.teamId = :teamId AND a.problemId = :problemId AND a.action = 'SEND_ANSWER'",
+    )
+    fun countActionsByTeamIdAndProblemIdAndAction(teamId: Long, problemId: Long): Long
+
+    @Query(
+        "SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END " +
+            "FROM Actions a " +
+            "WHERE a.teamId = :teamId AND a.problemId = :problemId AND a.action = 'SEND_ANSWER' " +
+            "AND a.isCorrectAnswer = false",
+    )
+    fun countByTeamIdAndProblemIdAndActionAndIsCorrectAnswer(teamId: Long, problemId: Long): Boolean
+}

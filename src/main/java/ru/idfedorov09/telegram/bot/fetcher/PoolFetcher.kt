1e3510a9677775b6a33572e8a1d6ecb9d734defe
The_Prax <qwerty202020101010@gmail.com>
2023-09-28 05:36:00 +0300

    mda1

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/PoolFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/PoolFetcher.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import ru.idfedorov09.telegram.bot.data.enums.ResponseAction
 import ru.idfedorov09.telegram.bot.data.model.UserResponse
+import ru.idfedorov09.telegram.bot.data.repo.ActionRepository
 import ru.idfedorov09.telegram.bot.data.repo.ProblemRepository
 import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
 import ru.idfedorov09.telegram.bot.flow.InjectData
@@ -13,12 +14,14 @@ import ru.idfedorov09.telegram.bot.service.UserInfoService
 class PoolFetcher(
     private val userInfoService: UserInfoService,
     private val problemRepository: ProblemRepository,
+    private val actionRepository: ActionRepository,
 ) : GeneralFetcher() {
 
     @InjectData
     fun doFetch(
         userResponse: UserResponse,
         bot: TelegramPollingBot,
+
     ) {
         if (userResponse.action != ResponseAction.GET_POOL) {
             return
@@ -30,12 +33,13 @@ class PoolFetcher(
             bot.execute(SendMessage(tui, "В пуле нет задач"))
             return
         }
+        val team = userResponse.initiatorTeam ?: return
         var i = 1
         var answerMessage = "Задачи в пуле:"
         problemsPool.forEach { probId ->
             val problem = problemRepository.findById(probId).get()
-            answerMessage += "\n$i. '${problem.category} ${problem.cost}' (ЛУЛ/2)"
-            // добавить вместо ЛУЛ счётчик попыток(сколько осталось)
+            answerMessage += "\n$i. '${problem.category} ${problem.cost}' " +
+                    "(${actionRepository.countActionsByTeamIdAndProblemIdAndAction(team.id, probId)}/2)"
             i++
         }
         bot.execute(SendMessage(tui, answerMessage).also { it.enableMarkdown(true) })

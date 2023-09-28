406eb9b1826c4731a8a19a6aac64a3d4084c04f9
Ivan Fedorov <idfedorov09@gmail.com>
2023-09-28 21:28:44 +0300

    refactoring

--- src/main/java/ru/idfedorov09/telegram/bot/SnoBotHandlerApplication.java
+++ src/main/java/ru/idfedorov09/telegram/bot/SnoBotHandlerApplication.java
@@ -3,8 +3,10 @@ package ru.idfedorov09.telegram.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableAsync
 public class SnoBotHandlerApplication {
 

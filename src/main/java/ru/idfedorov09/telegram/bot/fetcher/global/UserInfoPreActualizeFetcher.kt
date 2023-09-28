3fb18cc6771b21caa9ca199ce84388880a359984
Ivan Fedorov <idfedorov09@gmail.com>
2023-09-29 02:41:47 +0300

    done.

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/global/UserInfoPreActualizeFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/global/UserInfoPreActualizeFetcher.kt
@@ -1,10 +1,11 @@
 package ru.idfedorov09.telegram.bot.fetcher.global
 
-import org.hibernate.Hibernate
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery
 import org.telegram.telegrambots.meta.api.objects.Update
 import ru.idfedorov09.telegram.bot.data.model.UserInfo
 import ru.idfedorov09.telegram.bot.data.repo.UserInfoRepository
+import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
 import ru.idfedorov09.telegram.bot.fetcher.GeneralFetcher
 import ru.idfedorov09.telegram.bot.flow.ExpContainer
 import ru.idfedorov09.telegram.bot.flow.InjectData
@@ -20,12 +21,21 @@ class UserInfoPreActualizeFetcher(
         update: Update,
         updatesUtil: UpdatesUtil,
         exp: ExpContainer,
+        bot: TelegramPollingBot,
     ): UserInfo? {
         val chatId = updatesUtil.getChatId(update) ?: run {
             exp.hasChatId = false
             return null
         }
         exp.hasChatId = true
+
+        if (update.hasCallbackQuery()) {
+            bot.execute(
+                AnswerCallbackQuery().also {
+                    it.callbackQueryId = update.callbackQuery.id
+                },
+            )
+        }
 
         return userInfoRepository.findByTui(chatId)
             ?: UserInfo(tui = chatId).also { userInfoRepository.save(it) }

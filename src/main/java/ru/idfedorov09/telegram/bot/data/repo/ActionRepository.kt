df369f9c9820d74258b73cb8be1a5f19f9a9dd94
andromedagalua <andromedagalua@yandex.ru>
2023-09-28 04:51:41 +0300

    исправил название таблицы

--- src/main/java/ru/idfedorov09/telegram/bot/data/repo/ActionRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/repo/ActionRepository.kt
@@ -10,14 +10,14 @@ interface ActionRepository : JpaRepository<Action, Long> {
 
     @Query(
         "SELECT COUNT(a) " +
-            "FROM actions a " +
+            "FROM actions_table a " +
             "WHERE a.teamId = :teamId AND a.problemId = :problemId AND a.action = 'SEND_ANSWER'",
     )
     fun countActionsByTeamIdAndProblemIdAndAction(teamId: Long, problemId: Long): Long
 
     @Query(
         "SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END " +
-            "FROM Actions a " +
+            "FROM actions_table a " +
             "WHERE a.teamId = :teamId AND a.problemId = :problemId AND a.action = 'SEND_ANSWER' " +
             "AND a.isCorrectAnswer = false",
     )

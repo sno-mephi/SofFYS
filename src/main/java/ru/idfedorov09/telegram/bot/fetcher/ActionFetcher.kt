2c61d92c68c5cf20756190186400f1c76c1af67f
The_Prax <qwerty202020101010@gmail.com>
2023-09-28 03:34:22 +0300

    action1123

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/ActionFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/ActionFetcher.kt
@@ -5,17 +5,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.Update
 import ru.idfedorov09.telegram.bot.data.model.Action
-import ru.idfedorov09.telegram.bot.data.model.UserResponse
 import ru.idfedorov09.telegram.bot.data.model.IsAnswer
+import ru.idfedorov09.telegram.bot.data.model.UserResponse
 import ru.idfedorov09.telegram.bot.data.repo.ActionRepository
-import ru.idfedorov09.telegram.bot.data.repo.TeamRepository
-import ru.idfedorov09.telegram.bot.data.repo.UserInfoRepository
 import ru.idfedorov09.telegram.bot.executor.TelegramPollingBot
 import ru.idfedorov09.telegram.bot.flow.ExpContainer
 import ru.idfedorov09.telegram.bot.flow.InjectData
-import ru.idfedorov09.telegram.bot.service.RedisService
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 @Component
 class ActionFetcher(
@@ -31,10 +26,8 @@ class ActionFetcher(
         bot: TelegramPollingBot,
         exp: ExpContainer,
         update: Update,
-        isAnswer: IsAnswer
+        isAnswer: IsAnswer,
     ) {
-
-
         val action = Action(
             teamId = userResponse.initiatorTeam?.id,
             time = userResponse.receiveTime,
